5c5f68ddcaefbe15fa79d65d7c9ddf5021230b00
Javier Crafts <137738041+JavierCraft@users.noreply.github.com>
2024-07-03 10:41:15 +0700

    Add files via upload

---
+++ Cobacobaa/app/src/main/java/com/polytechnic/astra/ac/id/cobacobaa/CaptureActivityPortrait.java
@@ -0,0 +1,7 @@
+package com.polytechnic.astra.ac.id.cobacobaa;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+    // This class is required for locking the orientation in portrait mode
+}
---
+++ Cobacobaa/app/src/main/java/com/polytechnic/astra/ac/id/cobacobaa/MainActivity.java
@@ -0,0 +1,49 @@
+package com.polytechnic.astra.ac.id.cobacobaa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonScan = findViewById(R.id.button_scan);
+        buttonScan.setOnClickListener(v -> startBarcodeScanner());
+    }
+
+    private void startBarcodeScanner() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("Scan a barcode or QR Code");
+        integrator.setOrientationLocked(true);
+        integrator.setCaptureActivity(CaptureActivityPortrait.class);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
